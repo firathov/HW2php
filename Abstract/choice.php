803f8bde66f5e021fab4c62b7210b9fe194b2a1e
firathov <firat.hov@gmail.com>
2021-08-05 03:43:51 +0400

    probably end

---
+++ Abstract/choice.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AYE</title>
+</head>
+<body>
+
+<h2>Student or Aspirant?</h2>
+
+<p><a href="student.php">Student</a></p>
+
+<p><a href="aspirant.php">Aspirant</a></p>
+
+</body>
+</html>
--- Interface/interface.php
+++ Interface/interface.php
@@ -1,53 +1,97 @@
+<head>
+    <title>The Queen</title>
+</head>
+<h3>Queen - We are the champions</h3>
+<form method ="POST">
+    <p>The number of Guitar chords : <input type="number" name="guitar" required/></p>
+    <p>The size of Drum: <input type="number" name="drum" required><p/>
+    <p>The Microphone's decibel: <input type="number" name="microphone" required></p>
+    <input type = "submit" value="START THE SHOW!!!">
+</form>
 <?php
-interface iTool
+if(isset($_POST['guitar']) && isset($_POST['drum']) && isset($_POST['microphone']))
 {
-    function play();
-}
-class Guitar implements iTool
-{
-    public $key = "do major";
-    public $chordQuantity;
-    function __construct($key, $chordQuantity)
+    $guitar=htmlentities($_POST['guitar']);
+    $drum=htmlentities($_POST['drum']);
+    $microphone=htmlentities($_POST['microphone']);
+    if (is_numeric($guitar) && is_numeric($drum) && is_numeric($microphone))
     {
-        $this->key=(string)$key;
-        $this->chordQuantity=(int)$chordQuantity;
-    }
+        interface iTool
+        {
+            function play();
+        }
+        class Guitar implements iTool
+        {
+            public $key = "do major";
+            public $chordQuantity;
+            function __construct($key, $chordQuantity)
+            {
+                $this->key=(string)$key;
+                $this->chordQuantity=(int)$chordQuantity;
+            }
 
-    function play()
+            function play()
+            {
+                // TODO: Implement play() method.
+                echo "$this->key Brian May is playing with $this->chordQuantity chords guitar<br/>";
+            }
+        }
+        class Drum implements iTool
+        {
+            public $size;
+            function __construct($size)
+            {
+                $this->size=(float)$size;
+            }
+            function play()
+            {
+                // TODO: Implement play() method.
+                echo "Roger Taylor is playing with $this->size size drum<br/>";
+            }
+        }
+        class Microphone implements iTool
+        {
+            public $decibel;
+            function __construct($decibel)
+            {
+                $this->decibel=(float)$decibel;
+            }
+            public function play()
+            {
+                // TODO: Implement play() method.
+                echo "Freddie Mercury singing with $this->decibel decibel micro<br/><br/>";
+            }
+            public function sings()
+            {
+                echo "I've paid my dues<br/>
+                        Time after time<br/>
+                        I've done my sentence<br/>
+                        But committed no crime<br/>
+                        And bad mistakes<br/>
+                        I've made a few<br/>
+                        I've had my share of sand<br/>
+                        Kicked in my face<br/>
+                        But I've come through<br/>
+                        And we mean to go on and on and on and on<br/>
+                        We are the champions, my friends<br/>
+                        And we'll keep on fighting till the end<br/>
+                        We are the champions<br/>
+                        We are the champions<br/>
+                        No time for losers<br/>
+                        'Cause we are the champions of the World";
+            }
+        }
+        $guitar = new Guitar ("Do major<br/>", $_POST['guitar']);
+        $drum = new Drum($_POST['drum']);
+        $microphone = new Microphone($_POST['microphone']);
+        $guitar->play();
+        $drum->play();
+        $microphone->play();
+        echo '<p><a href="https://www.youtube.com/watch?v=KXw8CRapg7k">Click here to Listen</a></p>';
+        $microphone->sings();
+    }
+    else
     {
-        // TODO: Implement play() method.
-        echo "$this->key Guitar is playing with $this->chordQuantity<br/>";
+        echo "pls dont ruin and try again with numbers";
     }
 }
-class Drum implements iTool
-{
-    public $size;
-    function __construct($size)
-    {
-        $this->size=(float)$size;
-    }
-    function play()
-    {
-        // TODO: Implement play() method.
-        echo "Drum is playing with size $this->size<br/>";
-    }
-}
-class Trumpet implements iTool
-{
-    public $diametr;
-    function __construct($diameter)
-    {
-        $this->diametr=(float)$diameter;
-    }
-    public function play()
-    {
-        // TODO: Implement play() method.
-        echo "Trumpet is playing with size $this->diametr<br/>";
-    }
-}
-$guitar = new Guitar ("Do major", 5);
-$drum = new Drum(10);
-$trumpet = new Trumpet(15);
-$guitar->play();
-$drum->play();
-$trumpet->play();
